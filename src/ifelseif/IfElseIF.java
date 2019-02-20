5eb291b9728962029c4e740691b8ee9261ba40a0
sanduni-E <sanduniue@gmail.com>
2019-02-20 23:46:49 +0530

    If-Else-If
    If-Else-If

---
+++ src/ifelseif/IfElseIF.java
@@ -0,0 +1,21 @@
+
+package ifelseif;
+
+
+public class IfElseIF {
+
+    
+    public static void main(String[] args) {
+       int num=5;
+       if(num>6){
+           System.out.println("number is greater than 5");
+       }
+       else if(num<4){
+           System.out.println("number is less than 5");
+       }
+       else{
+           System.out.println("number is equal to 5");
+       }
+    }
+    
+}
